f770e489d398c0f6565fb2e07d9b276883ca8a38
Ari Lerner <arilerner@mac.com>
2015-05-12 12:14:34 -0700

    Initial commit

---
+++ RNAudioPlayer/RNAudioPlayer.h
@@ -0,0 +1,13 @@
+//
+//  RNAudioPlayer.h
+//  RNAudioPlayer
+//
+//  Created by Ari Lerner on 5/12/15.
+//  Copyright (c) 2015 Ari Lerner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNAudioPlayer : NSObject
+
+@end
---
+++ RNAudioPlayer/RNAudioPlayer.m
@@ -0,0 +1,13 @@
+//
+//  RNAudioPlayer.m
+//  RNAudioPlayer
+//
+//  Created by Ari Lerner on 5/12/15.
+//  Copyright (c) 2015 Ari Lerner. All rights reserved.
+//
+
+#import "RNAudioPlayer.h"
+
+@implementation RNAudioPlayer
+
+@end
